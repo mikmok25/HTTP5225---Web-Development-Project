d54b3930baae7f545d8374a89321a5316a297ec5
mikmok25 <mnmmanuit.student@ua.edu.ph>
2024-07-04 19:45:35 -0400

    Initial Commit for Assignment1

---
+++ Assignment1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pc Builder</title>
+</head>
+<body>
+
+    <h1>Welcome to PC builder!</h1>
+
+    <a href="cpu.php">CPUs</a>
+    <a href="gpu.php">GPUs</a>
+    <a href="ram.php">GPUs</a>
+    <a href="storage.php">GPUs</a>
+    
+</body>
+</html>
---
+++ Assignment1/partials/nav.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="cpu.php">CPUs</a></li>
+            <li><a href="gpu.php">GPUs</a></li>
+            <li><a href="ram.php">RAMs</a></li>
+            <li><a href="storage.php">Storage</a></li>
+        </ul>
+    </nav>
+</header>
