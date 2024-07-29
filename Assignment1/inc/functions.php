d448f8d02243e8168fda405d3f6715b82913618a
Mick Nixon <103007309+mikmok25@users.noreply.github.com>
2024-07-29 17:24:39 -0400

    Delete Assignment1 directory

--- Assignment1/inc/functions.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-function formatStorage($sizeInGB) {
-    if ($sizeInGB >= 1000) {
-        return $sizeInGB / 1000 . 'TB';  // Converts GB to TB for sizes 1000GB and above
-    }
-    return $sizeInGB . ' GB';
-}
